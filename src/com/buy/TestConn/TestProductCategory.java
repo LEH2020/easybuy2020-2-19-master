8a2a02362cc8a2bceb692ce0c0bdf1ee38ce9cb1
leh <2789948498@qq.com>
2020-02-20 12:07:44 +0800

    1.编写获取商品分类信息的service层代码及其实现类 2.完成数据测试

--- src/com/buy/TestConn/TestProductCategory.java
+++ src/com/buy/TestConn/TestProductCategory.java
@@ -3,6 +3,8 @@ package com.buy.TestConn;
 import com.buy.dao.product.IProductCategory;
 import com.buy.dao.product.ProductCategoryImpl;
 import com.buy.entity.EasybuyProductCategory;
+import com.buy.service.product.IProductCategoryService;
+import com.buy.service.product.ProductCategoryServiceImpl;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,8 +12,8 @@ import java.util.List;
 public class TestProductCategory{
 @Test
     public void testProductCategory(){
-        IProductCategory productCategory=new ProductCategoryImpl();
-        List<EasybuyProductCategory> productCategories=productCategory.queryAllPorductCategory("0");
+    IProductCategoryService service=new ProductCategoryServiceImpl();
+        List<EasybuyProductCategory> productCategories=service.queryAllPorductCategory("0");
         for(EasybuyProductCategory category : productCategories)
         {
             System.out.println(category.getName());
